a8903f7526a7ec159a417ebf633e5a02cfca993d
Todd Parsons <todd.parsons.mi@gmail.com>
2025-02-12 14:54:42 -0600

    Clearing out some of the initial stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Dog Matcher!</h1>
   </main>
 </template>
