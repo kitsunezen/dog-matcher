097845466ad242e060782b1e4d108ed0f793cd1a
Todd Parsons <todd.parsons.mi@gmail.com>
2025-02-13 15:14:05 -0600

    Getting tired of these 401's!

--- src/views/BreedsView.vue
+++ src/views/BreedsView.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-// import { useDogStore } from '@/stores/dogs'
+import { useDogStore } from '@/stores/dogs'
 // import BreedCard from '@/components/dogs/BreedCard.vue'
 
-// const dogs = useDogStore()
-// const breeds = dogs.getBreeds()
-// const firstBreed = ref(breeds[0])
-const test = ref(`Breeds Test!`)
+const dogs = useDogStore()
+const breeds = dogs.getBreeds()
+const firstBreed = ref(breeds[0])
+// const test = ref(`Breeds Test!`)
 </script>
 
 <template>
   <div>
-    <v-card :text="test" />
+    <v-card :text="firstBreed" />
   </div>
 </template>
 
